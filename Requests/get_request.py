98410361780459550133e3f33f16abcc7d689f02
dinesh <dineshsutharxii@gmail.com>
2025-02-15 11:28:11 +0530

    Added Api testing cases

---
+++ Requests/get_request.py
@@ -0,0 +1,16 @@
+import json
+import pytest
+import requests
+import time
+
+url_jsonpath = "https://jsonplaceholder.typicode.com/posts"
+url_gorest = "https://gorest.co.in/public/v2/users"
+
+response = requests.get(url_gorest)
+json_response = response.json()
+with open("respone.json", "w+") as res:
+    json.dump(json_response, res)
+with open("respone.json", "r+") as res:
+    data = json.load(res)
+print(data)
+print(json.dumps(data, indent=4)) # pretty format
