4c6ee25679058a3b4a5d8b22008900034a0d6a4c
dinesh <dineshsutharxii@gmail.com>
2025-02-24 07:32:55 +0530

    added more testcases

--- UnitTest/test_find_solution.py
+++ UnitTest/test_find_solution.py
@@ -8,6 +8,7 @@ def test_twosum():
     assert twoSum([2, 7, 11, 15], 9) == [0, 1]
     assert twoSum([3, 2, 4], 6) == [1, 2]
     assert twoSum([3, 3], 6) == [0, 1]
+    assert twoSum([3, 3, 7, 6], 13) == [2, 3]
 
 
 def test_minSubArrayLen():
