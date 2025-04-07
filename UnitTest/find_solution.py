792267cf06cee9217616e03030243168a0fceff7
dinesh <dineshsutharxii@gmail.com>
2025-04-07 10:37:43 +0530

    added changes to code

--- UnitTest/find_solution.py
+++ UnitTest/find_solution.py
@@ -486,10 +486,10 @@ class Solution:
     def hourglassSum(self, arr):
         l = len(arr)
         max_sum = float('-inf')
-        for i in range(l-2):
-            for j in range(l-2):
-                hourglass_sum =(arr[i][j]+ arr[i][j+1]+ arr[i][j+2] +
-                                arr[i+1][j+1] +
-                                arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+        for i in range(l - 2):
+            for j in range(l - 2):
+                hourglass_sum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                                 arr[i + 1][j + 1] +
+                                 arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2])
                 max_sum = max(max_sum, hourglass_sum)
         return max_sum
