1995cd6a50a7b2a44b8077e0b5096ba28a99dda0
dinesh <dineshsutharxii@gmail.com>
2025-05-13 08:49:50 +0530

    Added more changes

--- UnitTest/test_find_solution.py
+++ UnitTest/test_find_solution.py
@@ -396,15 +396,17 @@ def test_minSum():
 
 def test_threeConsecutiveOdds():
     print(' --- Inside test_threeConsecutiveOdds ---')
-    assert Solution().threeConsecutiveOdds([2,6,4,1]) == False
-    assert Solution().threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]) == True
+    assert Solution().threeConsecutiveOdds([2, 6, 4, 1]) == False
+    assert Solution().threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]) == True
+
 
 def test_findScore():
     print(' --- Inside test_findScore ---')
-    assert Solution().findScore([2,1,3,4,5,2]) == 7
-    assert Solution().findScore([2,3,5,1,3,2]) == 5
+    assert Solution().findScore([2, 1, 3, 4, 5, 2]) == 7
+    assert Solution().findScore([2, 3, 5, 1, 3, 2]) == 5
+
 
 def test_lengthAfterTransformations():
     print(' --- Inside test_lengthAfterTransformations ---')
-    assert Solution().lengthAfterTransformations("abcyy", 2 ) == 7
+    assert Solution().lengthAfterTransformations("abcyy", 2) == 7
     assert Solution().lengthAfterTransformations("azbk", 1) == 5
