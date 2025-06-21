a3b8ea4a62b82e1e5c144320739e019b5a04fd72
dinesh <dineshsutharxii@gmail.com>
2025-06-21 09:55:41 +0530

    practice question

--- UnitTest/test_find_solution.py
+++ UnitTest/test_find_solution.py
@@ -600,9 +600,11 @@ def test_numOfSubarraysOptimize():
     assert Solution().numOfSubarrays([2, 4, 6]) == 0
     assert Solution().numOfSubarrays([1, 2, 3, 4, 5, 6, 7]) == 16
 
+
 def test_partitionArray():
     print(' --- Inside test_numOfSubarrays ---')
-    assert Solution().partitionArray([3,6,1,2,5], 2) == 2
+    assert Solution().partitionArray([3, 6, 1, 2, 5], 2) == 2
+
 
 def test_minimumDeletions():
     print(' --- Inside test_numOfSubarrays ---')
