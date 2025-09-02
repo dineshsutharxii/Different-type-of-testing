7366647baa356277050e1fbcfae4b5f4de8a4221
dinesh <dineshsutharxii@gmail.com>
2025-09-02 18:50:46 +0530

    Added more leetcode solution

--- UnitTest/find_solution.py
+++ UnitTest/find_solution.py
@@ -1839,3 +1839,19 @@ class Solution:
             delta = (p + 1) / (t + 1) - p / t
             heapq.heappush(h, (-delta, p, t))
         return round(sum(p / t for _, p, t in h) / len(classes), 5)
+
+    def numberOfPairs(self, points):
+        points.sort(key=lambda x: (x[0], -x[1]))
+        pair_cnt = 0
+        n = len(points)
+        for i in range(n):
+            upper_y = points[i][1]
+            lower_y_limit = float('-inf')
+            for j in range(i + 1, n):
+                current_y = points[j][1]
+                if current_y <= upper_y and current_y > lower_y_limit:
+                    pair_cnt += 1
+                    lower_y_limit = current_y
+                    if current_y == upper_y:
+                        break
+        return pair_cnt
