e661c8e8b1975f4a0d2b73fb1ae1b1d127ef64e4
dinesh <dineshsutharxii@gmail.com>
2026-07-19 21:41:00 +0530

    Day 2

---
+++ practice_dsa.py
@@ -0,0 +1,36 @@
+#Find all the elements of list which contains word 'w'
+def find_elements_in_list_which_contains_word(listy, w):
+    for ele in listy:
+        l_ptr, w_ptr = 0, 0
+        while l_ptr < len(ele) and w_ptr < len(w):
+            if w[w_ptr] == ele[l_ptr]:
+                w_ptr += 1
+                l_ptr += 1
+            else:
+                l_ptr += 1
+        if w_ptr == len(w):
+            print(f"{w} is present in {ele}")
+
+
+listy = ['sfskwomfjprifse', 'koomprise', 'jkomprisxmsl', 'saxuuywq', 'komprise']
+w = "komprise"
+find_elements_in_list_which_contains_word(listy, w)
+
+
+def bin_search(_arr, ele):
+    l, u = 0, len(_arr) - 1
+    while l <= u:
+        m = (l + u) // 2
+        if ele == _arr[m]:
+            return True
+        if ele > _arr[m]:
+            l = m + 1
+        else:
+            u = m - 1
+    return False
+
+
+_arr = [1, 5, 6, 9, 11, 12, 18]
+ele, ele1 = 18, 15
+print(f'is {ele} present : ', bin_search(_arr, ele))
+print(f'is {ele1} present : ', bin_search(_arr, ele1))
