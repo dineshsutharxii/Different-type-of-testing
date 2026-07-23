a741c407f694eba23e19b3f5e8b7c42911db7635
dinesh <dineshsutharxii@gmail.com>
2026-07-23 19:42:20 +0530

    Day5

--- practice_python.py
+++ practice_python.py
@@ -72,20 +72,21 @@ print(len(even))
 ##oops
 #class and object
 class Car:
-    company = "Honda" #class variable
-    def __init__(self, name):
-        self.name = name # instance variable
+    company = "Honda"  #class variable
 
-    def display(self): #instance method
+    def __init__(self, name):
+        self.name = name  # instance variable
+
+    def display(self):  #instance method
         print(self.name)
 
     @classmethod
-    def change_name(cls, name): #class methods
+    def change_name(cls, name):  #class methods
         cls.company = name
 
     @staticmethod
-    def add_two_number(a, b):
-        return a+b
+    def add_two_number(a, b):  #static method
+        return a + b
 
 
 car = Car('Civic')
@@ -93,6 +94,44 @@ car.display()
 print(Car.company)
 car.change_name('BMW')
 print(car.company)
-print(Car.add_two_number(1,4))
-print(car.add_two_number(4,2))
+print(Car.add_two_number(1, 4))
+print(car.add_two_number(4, 2))
 
+
+#encapsulation
+# __var - private variable : A public variable can be accessed from anywhere.
+# _var - protected variable : Protected variables are meant to be used inside the class and its subclasses.
+# var - public variable : It appears inaccessible from outside the class. Double underscore triggers name mangling.
+#Python internally changes the variable name.
+#__svar becomes _classnaem__var  This is called name mangling.
+class Bank:
+    def __init__(self):
+        self.__balance = 1000
+
+    def get_balance(self):
+        return self.__balance
+
+
+bank = Bank()
+print(bank.get_balance())
+
+
+class Employee:
+
+    def __init__(self):
+        self.__salary = 50000
+
+
+emp = Employee()
+print(emp.__dict__)
+print(emp._Employee__salary)  #mangling effect
+
+#Inheritance
+class Animal:
+    def sound(self):
+        print("Animal")
+class Dog(Animal):
+    pass
+
+dog = Dog()
+dog.sound()
