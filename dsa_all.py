29074b4e4106c7376a863575117ca17eae5cbda2
dinesh <dineshsutharxii@gmail.com>
2026-08-15 18:16:38 +0530

    dsa Day1

--- dsa_all.py
+++ dsa_all.py
@@ -7,3 +7,13 @@ def two_sum(arr_, target):
         seen[val] = idx
     return []
 print(two_sum([1,2,3,4,5], 7))
+
+def contains_duplicate(arr):
+    seen = set()
+    for ele in arr:
+        if ele in seen:
+            return [ele, True]
+        seen.add(ele)
+    return []
+
+print(contains_duplicate([1,2,5,4,5]))
