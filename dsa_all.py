f060bb0d6cc0423b56fa97072d934e3cd5893816
dinesh <dineshsutharxii@gmail.com>
2026-08-15 18:14:28 +0530

    dsa Day1

---
+++ dsa_all.py
@@ -0,0 +1,9 @@
+def two_sum(arr_, target):
+    seen = {}
+    for idx, val in enumerate(arr_):
+        diff = target - val
+        if diff in seen:
+            return [seen[diff], idx]
+        seen[val] = idx
+    return []
+print(two_sum([1,2,3,4,5], 7))
