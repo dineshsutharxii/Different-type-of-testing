305f9888ada1f6c779241d55f206abf3ff8f9a6a
dinesh <dineshsutharxii@gmail.com>
2026-08-17 10:14:36 +0530

    dsa Day3

--- dsa_all.py
+++ dsa_all.py
@@ -49,4 +49,15 @@ print(max_subarray([-2, -1, 0, 1, 2]))
 
 def max_subarray_optimized(arr):
     max_sum = float('-inf')
+    current_sum = 0
+    for i in range(len(arr)):
+        if current_sum == 0:
+            start = i
+        current_sum += arr[i]
+        if current_sum > max_sum:
+            max_sum = current_sum
+        if current_sum < 0:
+            current_sum = 0
+    return max_sum
 
+print(max_subarray([-2, -3, 4, -1, -2, 1, 5, -3]))
